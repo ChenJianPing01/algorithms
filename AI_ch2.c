ba94316c749555bc3d0c399e399aee48543fa1f4
ChenJianPing01 <361512960@qq.com>
2018-10-23 17:06:37 +0800

    queens updated.

--- AI_ch2.c
+++ AI_ch2.c
@@ -3,24 +3,24 @@
 #include <limits.h>
 #include <stdio.h>
 #include <string.h>
-#define QUEENSIZE 4
 
-int queens[QUEENSIZE * QUEENSIZE];  // 皇后布置棋盘，一维数组表示
-int queens_cover[QUEENSIZE * QUEENSIZE];  // 该位置是否已被皇后辐射 1:是 0:否
+#define size 4
+int queens[size * size];  // 皇后布置棋盘，一维数组表示
+int queens_cover[size * size];  // 该位置是否已被皇后辐射 1:是 0:否
 // 初始化
 void queens_init(void) {
-    for (int i = 0; i < QUEENSIZE * QUEENSIZE; i++) {
+    for (int i = 0; i < size * size; i++) {
         queens[i] = 0;
     }
 }
 void queens_cover_init(void) {
-    for (int i = 0; i < QUEENSIZE * QUEENSIZE; i++) {
+    for (int i = 0; i < size * size; i++) {
         queens_cover[i] = 0;
     }
 }
 
 //字符串输出
-char * queens_str(int queens[], int size)
+char * queens_str()
 {
     int i, r, c, len = 2;
     sprintf(str, "  ");
@@ -42,7 +42,7 @@ char * queens_str(int queens[], int size)
 }
 
 // 根据皇后放入的位置，更新辐射位置数组
-void gen_queen_rc(int queens_cover[], int size, int row, int col)
+void update_cover(int row, int col)
 {
     int r, c;
     // 所在列
@@ -71,18 +71,58 @@ void gen_queen_rc(int queens_cover[], int size, int row, int col)
     }
 }
 
+// 生成中间状态。在某列放入皇后，放入前判断是否被辐射
+int insert_queen(int col) {
+    for (int r = 0; r < size; r++) {
+        if (queens_cover[r * size + col] == 0) {
+            queens[r * size + col] = 1;
+            return 1;
+        }
+    }
+    return 0;
+}
+
+// 判断皇后是否达到数量
+int isok() {
+    int count = 0;
+    for (int i = 0; i < size * size; i++) {
+        count += queens[i];
+    }
+    return count == size ? 1 : 0;
+}
+
+// 生成与测试
+int genner(int queens[], int size) { 
+    int row = 0, col = 0;   
+    queens_init();
+    queens_cover_init();    
+    while (!isok() && col < size) {
+        while (row < size) {
+            if (insert_queen(col)) {  // 插入皇后
+                update_cover(row, col); // 更新覆盖范围                
+            } else {
+                break;
+            }
+        }
+        col++;
+    }
+    if (isok()) {
+        return 1;
+    }
+}
+
 int main(void)
 {
     queens_init();
     queens_cover_init();
-    printf("queens:\n%s", queens_str(queens, QUEENSIZE));
-    printf("cover:\n%s\n\n", queens_str(queens_cover, QUEENSIZE));
+    printf("queens:\n%s", queens_str(queens, size));
+    printf("cover:\n%s\n\n", queens_str(queens_cover, size));
 
     /*
-    for (int r = 0; r < QUEENSIZE; r++) {
-        for (int c = 0; c < QUEENSIZE; c++) {
-            gen_queen_rc(queens_cover, QUEENSIZE, r, c);
-            printf("(%d, %d)cover:\n%s\n", r + 1, c + 1, queens_str(queens_cover, QUEENSIZE));
+    for (int r = 0; r < size; r++) {
+        for (int c = 0; c < size; c++) {
+            gen_queen_rc(queens_cover, size, r, c);
+            printf("(%d, %d)cover:\n%s\n", r + 1, c + 1, queens_str(queens_cover, size));
             queens_cover_init();
         }
     }
